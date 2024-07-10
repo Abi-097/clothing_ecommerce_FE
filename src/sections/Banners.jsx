eb7155e844ccd2eabcc05714639cd44122c4b874
Abi-097 <120549516+Abi-097@users.noreply.github.com>
2024-07-10 23:01:10 +0530

    banner added

--- src/sections/Banners.jsx
+++ src/sections/Banners.jsx
@@ -19,7 +19,20 @@ const Banners = () => {
       className={`${
         darkMode ? "dark bg-black" : "light bg-white"
       } w-full lg:px-20 px-10 lg:py-20 py-10 flex lg:flex-row flex-col justify-center items-center gap-6 `}
-    ></section>
+    >
+      <div className="lg:w-[50%] w-full ">
+        <img src={banner2} alt="banner2" data-aos="zoom-in" />
+      </div>
+      <div className="lg:w-[50%] lg:p-10 w-full flex flex-col justify-center align-center gap-8">
+        <h1
+          className="text-center uppercase text-2xl text-slate-500 dark:text-white line-clamp-5"
+          data-aos="zoom-in"
+          data-aos-delay="200"
+        >
+          upto 60% off
+        </h1>
+      </div>
+    </section>
   );
 };
 
